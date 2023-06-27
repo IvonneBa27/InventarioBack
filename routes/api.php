f0313287998587f6bd74b4326ff6398952fe2c40
Roman Madrigal <rmcentinela@gmail.com>
2023-06-27 11:14:20 -0600

    add route blacklist

--- routes/api.php
+++ routes/api.php
@@ -255,6 +255,12 @@ Route::post('transferStore/update', [transferStoreController::class, 'update']);
 Route::post('transferDetailStore/create',[transferStoreDetailController::class, 'create']);
 
 
+//  TODO: BLACKLIST ROUTES
+Route::get('blacklist/list', [BlackListController::class, 'index']);
+Route::post('blacklist/create', [BlackListController::class, 'create']);
+Route::post('blacklist/update', [BlackListController::class, 'update']);
+Route::get('blacklist/delete', [BlackListController::class, 'destroy']);
+
 // TODO: RUTAS ENTRENADOR
 // Route::get('/obtenerEntrenadores', [CoachController::class, 'index'])->middleware('auth:sanctum');
 // Route::post('/crearEntrenador', [CoachController::class, 'store']);
